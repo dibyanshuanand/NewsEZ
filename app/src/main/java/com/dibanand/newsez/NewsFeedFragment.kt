f8445bf12a6fc4bad5e36907da5cba18b1aa3732
dibyanshuanand <dibyanshuanand2000@gmail.com>
2023-02-12 08:39:20 +0530

    Setup news feed fragment, network calls

--- app/src/main/java/com/dibanand/newsez/NewsFeedFragment.kt
+++ app/src/main/java/com/dibanand/newsez/NewsFeedFragment.kt
@@ -1,59 +1,78 @@
 package com.dibanand.newsez
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dibanand.newsez.adapter.NewsListAdapter
+import com.dibanand.newsez.databinding.FragmentNewsFeedBinding
+import com.dibanand.newsez.ui.NewsViewModel
+import com.dibanand.newsez.util.ResourceState
+import com.google.android.material.snackbar.Snackbar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [NewsFeedFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NewsFeedFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var binding: FragmentNewsFeedBinding
+    private lateinit var viewModel: NewsViewModel
+    private lateinit var newsListAdapter: NewsListAdapter
+
+    companion object {
+        const val TAG = "NewsFeedFragment"
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news_feed, container, false)
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentNewsFeedBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NewsHeadlinesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NewsFeedFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.e(TAG, "onViewCreated: Called")
+        viewModel = ViewModelProvider(requireActivity()).get(NewsViewModel::class.java)
+        setupRecyclerView()
+
+        viewModel.newsHeadlines.observe(viewLifecycleOwner, Observer { response ->
+            when (response) {
+                is ResourceState.Success -> {
+                    response.data?.let { res ->
+                        newsListAdapter.newsList = res.articles.toMutableList()
+                    }
+                }
+                is ResourceState.Loading -> {
+                    Snackbar.make(binding.rvNewsHeadlines, "Loading", Snackbar.LENGTH_SHORT)
+                        .show()
+                }
+                is ResourceState.Error -> {
+                    response.message?.let {msg ->
+                        Snackbar.make(binding.rvNewsHeadlines, "Error: $msg", Snackbar.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+                is ResourceState.Undefined -> {
+                    // no-op
                 }
             }
+        })
+    }
+
+    private fun setupRecyclerView() {
+        newsListAdapter = NewsListAdapter()
+        binding.rvNewsHeadlines.apply {
+            adapter = newsListAdapter
+            layoutManager = LinearLayoutManager(activity)
+            // TODO: Handle pagination - scrolling
+        }
     }
 }
